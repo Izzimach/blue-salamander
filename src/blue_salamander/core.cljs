441113ab2039d12fea6ac9ca82e95f047ef44122
Gary Haussmann <gjhaussmann@gmail.com>
2016-04-30 23:34:28 -0600

    Add game ticking and player input processing

--- src/blue_salamander/core.cljs
+++ src/blue_salamander/core.cljs
@@ -4,8 +4,7 @@
             [om.next :as om :refer-macros [defui]]
             [om.dom :as dom]
             [blue-salamander.r3elements :as r3 :include-macros]
-            [blue-salamander.keypresses :as keypress]
-            [blue-salamander.r3utils :as r3u :include-macros]
+            [blue-salamander.keypresses :as k]
             [thi.ng.geom.core :as g]
             [thi.ng.geom.core.vector :as v :refer [vec2 vec3]]))
 
@@ -19,18 +18,26 @@
 ;;
 
 (defonce keys-pressed (atom #{}))
-(keypress/track-keypresses-in-atom keys-pressed)
+(k/track-keypresses-in-atom keys-pressed)
 
 
 ;;
 ;; define your app data so that it doesn't get over-written on reload
 ;;
 
-(defonce app-state (atom {:camerapos (vec3 400 200 400)
-                          :player/position (vec3 0 0 0)
+(def origin (vec3 0 0 0))
+(def unity (vec3 1 1 1))
+
+(defonce app-state (atom {:player/position (vec3 300 0 0)
                           :screen/width 600
                           :screen/height 400
-                          :count 1}))
+                          :count 1
+                          :blockdata [
+                                      {:position origin :size unity}
+                                      {:position (vec3 0 0 100) :size unity}
+                                      {:position (vec3 100 0 100) :size unity}
+                                      {:position (vec3 0 0 200) :size unity}
+                                      ]}))
 
 (defn read-fn
   [{:keys [state] :as env} key params]
@@ -51,7 +58,7 @@
 
 
 (def boxgeometry (js/THREE.BoxGeometry. 100 100 100))
-(def cupcaketexture (js/THREE.ImageUtils.loadTexture "assets/lollipopGreen.png"))
+(def cupcaketexture (js/THREE.ImageUtils.loadTexture "assets/cupCake.png"))
 (def cupcakematerial (js/THREE.MeshBasicMaterial. #js {:map cupcaketexture}))
 
 
@@ -68,34 +75,65 @@
          [:screen/width :screen/height :player/position])
   Object
   (render [this]
-          (let [{width :screen/width height :screen/height playerpos :player/position} (om/props this)
+          (let [{width :screen/width height :screen/height [px py pz] :player/position :as props} (om/props this)
+                ;; om.next destructures thi.ng vec3's for some reason
+                playerpos (vec3 px py pz)
                 aspect (/ width height)
                 lookat playerpos
-                camerapos  (g/+ (vec3 1 1 1) (vec3 400 200 200))
+                camerapos (g/+ playerpos (vec3 -200 400 -200))
                 curprops (assoc playercamera-defaultprops
                                 :aspect aspect
                                 :position (r3/vec3->Vector3 camerapos)
                                 :lookat (r3/vec3->Vector3 lookat))]
             (r3/perspectivecamera curprops))))
-(def playercamera (om/factory PlayerCamera {:keyfn :key}))
+(def playercamera (om/factory PlayerCamera))
+
+(defui PlayerCharacter
+  static om/IQuery
+  (query [this]
+         [:player/position])
+  Object
+  (render [this]
+          (let [{playerpos :player/position :as props} (om/props this)]
+            (r3/mesh {:position (r3/vec3->Vector3  playerpos)
+                      :geometry boxgeometry
+                      :material cupcakematerial}))))
+(def playercharacter (om/factory PlayerCharacter))
+
+(defn blockdata->meshprops [block]
+  (let [{:keys [position size]} block
+        [sx sy sz] size
+        scale (js/THREE.Vector3 (/ sx 100) (/ sy 100) (/ sz 100))]
+    {:position (r3/vec3->Vector3 position)
+     :geometry boxgeometry
+     :material cupcakematerial
+     :scale scale}))
+
+(defui LavaLand
+  Object
+  (render [this]
+          (let [{:keys [blockdata]} (om/props this)
+                datatomesh (comp r3/mesh blockdata->meshprops)]
+            (r3/object3d {}
+                         (mapv datatomesh blockdata)))))
+(def lava-land (om/factory LavaLand))
 
 (defui GameScreen
   static om/IQuery
   (query [this]
-         [:screen/width :screen/height :player/position])
+         [:screen/width :screen/height :player/position :blockdata])
   Object
   (render [this]
           (let [props (om/props this)
-                {width :screen/width height :screen/height} props
+                {width :screen/width height :screen/height playerpos :player/position} props
                 rendererprops {:width width :height height}
                 sceneprops (assoc rendererprops :camera "playercamera")]
             (r3/renderer rendererprops
                          (r3/scene sceneprops
-                                   [(playercamera props)
-                                    (r3/mesh {:key 'box'
-                                              :position r3/Origin
-                                              :geometry boxgeometry
-                                              :material cupcakematerial})])))))
+                                   [(playercamera (assoc props :key 'playercamera'))
+                                    (playercharacter {:key 'playercharacter'
+                                                      :player/position playerpos})
+                                    (lava-land props)])))))
 
 
 
@@ -109,6 +147,46 @@
               GameScreen
               (gdom/getElement "app"))
 
+(defn player-input []
+  (let [curkeys @keys-pressed
+        left (contains? curkeys :left-arrow)
+        right (contains? curkeys :right-arrow)
+        up (contains? curkeys :up-arrow)
+        down (contains? curkeys :down-arrow)
+        dx (cond
+             left 1
+             right -1
+             :else 0)
+        dy (cond
+             up 1
+             down -1
+             :else 0)]
+    [(+ dx dy) (- dy dx)]))
+
+(defn game-tick-fn
+  [state newtime]
+  (let [playerpos (:player/position state)
+        [dx dy] (player-input)
+        newposition (g/+ playerpos (vec3 dx 0 dy))]
+    (assoc state :player/position newposition)))
+
+(def tickID :off)
+
+(defn tick-fn [highrestime]
+  (do 
+    #_(js/console.log "tick")
+    (set! tickID (js/requestAnimationFrame tick-fn))
+    (swap! app-state game-tick-fn highrestime)))
+
+(defn start-ticking []
+  (if (= tickID :off)
+    (set! tickID (js/requestAnimationFrame tick-fn))))
+
+(defn stop-ticking []
+  (if-not (= tickID :off)
+    (do
+      (js/cancelAnimationFrame tickID)
+      (set! tickID :off))))
 
 (defn on-js-reload []
   ;; optionally touch your app-state to force rerendering depending on
--- src/blue_salamander/keypresses.cljs
+++ src/blue_salamander/keypresses.cljs
@@ -6,17 +6,25 @@
 ;; set. You pass in an atom that contains the (initially empty set) and
 ;; as keys are pressed/released their keycodes are swap!'d into and out of the set
 
+;; record keys as something more readable than numbers
+
+(def keypress-map
+  {37 :left-arrow
+   38 :up-arrow
+   39 :right-arrow
+   40 :down-arrow})
+
 ;; KeyboardEvent.keyCode is deprecated but .key doesn't seem
 ;; to be supported everywhere
 
 (defn process-key-down [keys-pressed-atom key-event]
   (let [key (.-keyCode key-event)]
-    (swap! keys-pressed-atom #(conj % key))
+    (swap! keys-pressed-atom #(conj % (get keypress-map key)))
     #_(js/console.log (str "key down: " key))))
 
 (defn process-key-up [keys-pressed-atom key-event]
   (let [key (.-keyCode key-event)]
-    (swap! keys-pressed-atom #(disj % key))
+    (swap! keys-pressed-atom #(disj % (get keypress-map key)))
     #_(js/console.log (str "key up: " key))))
 
 (defn track-keypresses-in-atom
@@ -24,3 +32,5 @@
   (let []
     (aset js/window "onkeydown" #(process-key-down keytrack-atom %))
     (aset js/window "onkeyup" #(process-key-up keytrack-atom %))))
+
+
