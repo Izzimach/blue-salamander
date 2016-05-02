e2d08d68f7eb25c7a8b6878a0fdcfba6e995d082
Gary Haussmann <gjhaussmann@gmail.com>
2016-05-01 23:11:11 -0600

    Keep track of keys in an atom in the keypresses namespace.

--- src/blue_salamander/keypresses.cljs
+++ src/blue_salamander/keypresses.cljs
@@ -9,28 +9,47 @@
 ;; record keys as something more readable than numbers
 
 (def keypress-map
-  {37 :left-arrow
-   38 :up-arrow
-   39 :right-arrow
-   40 :down-arrow})
+  {:left-arrow 37
+   :up-arrow 38
+   :right-arrow 39
+   :down-arrow 40})
+
+;;
+;; the keys-pressed atom contains currently pressed keys, stored
+;; as key codes.
+;;
+(def keys-pressed (atom #{}))
+
+(defn is-key-pressed?
+  "Returns true if the current key is held down. Can be one of the keywords listed in keypress-map or just a raw keycode if you know it"
+  [key]
+  (if (keyword? key)
+    (contains? @keys-pressed (get keypress-map key)) ;; map from keyword to keycode and check
+    (contains? @keys-pressed key))) ;; just use the raw keycode
 
 ;; KeyboardEvent.keyCode is deprecated but .key doesn't seem
 ;; to be supported everywhere
 
-(defn process-key-down [keys-pressed-atom key-event]
+(defn- process-key-down [keys-pressed-atom key-event]
   (let [key (.-keyCode key-event)]
-    (swap! keys-pressed-atom #(conj % (get keypress-map key)))
+    (swap! keys-pressed-atom #(conj % key))
     #_(js/console.log (str "key down: " key))))
 
-(defn process-key-up [keys-pressed-atom key-event]
+(defn- process-key-up [keys-pressed-atom key-event]
   (let [key (.-keyCode key-event)]
-    (swap! keys-pressed-atom #(disj % (get keypress-map key)))
+    (swap! keys-pressed-atom #(disj % key))
     #_(js/console.log (str "key up: " key))))
 
-(defn track-keypresses-in-atom
+(defn- track-keypresses-in-atom
   [keytrack-atom]
   (let []
     (aset js/window "onkeydown" #(process-key-down keytrack-atom %))
     (aset js/window "onkeyup" #(process-key-up keytrack-atom %))))
+
+(defn start-keypress-tracking
+  []
+  (track-keypresses-in-atom keys-pressed))
+
+
 
 
